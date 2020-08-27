d9b7224d179078f5221a525bb50c1b468755cb16
Andre Heslop <andreheslop@gmail.com>
2020-08-27 16:10:17 -0400

    added user connection

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user, class_name: "User", foreign_key: "created_by"
   validates_presence_of :artist_name, :album_title, :release_date, :record_label, :review_text, :img_url, :profile_pic_url
 end
